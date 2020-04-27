e80986eb3cd25b2598dc1b7af8299df709855642
Blake Scott <blakerscott@gmail.com>
2020-04-26 18:34:01 -0700

    add Title Component

---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//basic react component
+const Title = () => {
+    return (
+      <div>
+        <img src={require('./title.gif')} alt="meme"></img>
+      </div>
+    );
+  };
+
+export default Title;
